942a9ecbb872ba0ea81835649c215d7829f2d519
Arpit-3000 <arpitcollege1205@gmail.com>
2025-05-17 09:18:34 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,22 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  ssr: {
-    noExternal: ['@mediapipe/pose', '@mediapipe/camera_utils']
-  },
   css: {
     postcss: './postcss.config.js',
   },
   optimizeDeps: {
     include: [
       'html2canvas',
-      '@mediapipe/pose',
-      '@mediapipe/camera_utils'
     ]
   },
-  build: {
-    commonjsOptions: {
-      include: [/mediapipe/, /node_modules/]
-    }
-  }
+}
 });
